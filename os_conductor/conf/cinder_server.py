f0de1f792fcf4be908ec2927d5cae9a7dd1feb60
Fabian Baena <fabian.baena@samanagroup.co>
2019-04-09 16:29:59 -0400

    first commit

---
+++ os_conductor/conf/cinder_server.py
@@ -0,0 +1,62 @@
+#
+# Copyright 2019 Samana Group
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_config import cfg
+
+cinder_server_group = cfg.OptGroup('cinder_server',
+    title='Cinder Server options',
+    help="""
+        Options under this group are used to define Cinder configurations.
+        """)
+
+cinder_server_options = [
+    cfg.BoolOpt("enabled",
+        default=False,
+        help="""
+            Determines if Cinder Server configuration file will be handled.
+            """),
+
+    cfg.StrOpt("config_dir",
+        default="/etc/cinder",
+        help="""
+            Determine the root path for Cinder configuration files.
+            """),
+
+    cfg.StrOpt("config_files",
+        default="",
+        help="""
+            Coma separated list of configuration files to be generated.
+            e.g. cinder.conf
+            """),
+
+    cfg.StrOpt("group",
+        default="cinder",
+        help="""
+            Determines the group that will be allowed access to the configuration file.
+            """),
+
+    cfg.StrOpt("user",
+        default="cinder",
+        help="""
+            Determines the user that will be allowed access to the configuration file.
+            """)    
+]
+
+def register_opts(conf):
+    conf.register_group(cinder_server_group)
+    conf.register_opts(cinder_server_options, group=cinder_server_group)
+
+def list_opts():
+    return {cinder_server_group: cinder_server_options}
