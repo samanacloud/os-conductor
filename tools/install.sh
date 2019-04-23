811af7ce84483ed88111f13b665a9944d6de110f
Fabian Baena <fabian.baena@samanagroup.co>
2019-04-23 18:29:18 -0400

    first commit

---
+++ tools/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+mkdir -p /var/log/os_conductor
+mkdir -p /etc/os_conductor
+if [ ! -f /etc/os_conductor/os_conductor.conf ]; then
+    oslo-config-generator \
+        --config-file=$DIR/etc/os_conductor/os_conductor-config-generator.conf \
+        --output-file /etc/os_conductor/os_conductor.conf
+fi
+cp $DIR/os_conductor.service /lib/systemd/system
+cp $DIR/init.d/os_conductor /etc/init.d
+systemctl daemon-reload
