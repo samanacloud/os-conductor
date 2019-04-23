a4679a0c0ba2dc8ec7c4be1c1dc370b8112c2d50
Fabian Baena <fabian.baena@samanagroup.co>
2019-04-23 18:31:09 -0400

    first commit

--- tools/install.sh
+++ tools/install.sh
@@ -6,7 +6,7 @@ mkdir -p /var/log/os_conductor
 mkdir -p /etc/os_conductor
 if [ ! -f /etc/os_conductor/os_conductor.conf ]; then
     oslo-config-generator \
-        --config-file=$DIR/etc/os_conductor/os_conductor-config-generator.conf \
+        --config-file=$DIR/../etc/os_conductor/os_conductor-config-generator.conf \
         --output-file /etc/os_conductor/os_conductor.conf
 fi
 cp $DIR/os_conductor.service /lib/systemd/system
